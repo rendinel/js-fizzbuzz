6f7f890c896c0d7c6e696c45f25ec9184ee18400
lucio rendine <luciorendine69@gmail.com>
2020-12-17 16:33:27 +0100

    second push

--- js/script.js
+++ js/script.js
@@ -4,5 +4,21 @@ for (var i = 0; i < 100; i++) {
 }
 
 for (var i = 0; i < points.length; i++) {
-    console.log(points[i]); 
+    console.log(points[i]);
+    if (points[i] % 3 == 0 )  {
+      console.log('Fizz')
+    } else if (points[i] % 5 == 0) {
+      console.log('Buzz')
+    } else ((points[i] % 3 == 0) && (points[i] % 5 == 0)) {
+      console.log('BuzzFizz')
+    }
 }
+
+// for (var i = 0; i < points.length; i++) {
+// if (points[i] % 3 == 0 )  {
+//   console.log('Fizz')
+// } else if (points[i] % 5 == 0) {
+//   console.log('Buzz')
+// } else (points[i] % 3 == 0  &&  points[i] % 5 == 0) {
+//   console.log('BuzzFizz')
+// } }
